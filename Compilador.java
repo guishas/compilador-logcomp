018684dae48f36392e7aef325ac33778cc2fdc08
guishas <lunettagui@gmail.com>
2023-05-12 15:56:52 -0300

    v3.0.2

--- Compilador.java
+++ Compilador.java
@@ -6,8 +6,15 @@ import java.nio.file.Paths;
 public class Compilador {
     public static void main(String[] args) throws Exception {
         String content = Files.readString(Paths.get(args[0]).toAbsolutePath());
+        String fileName = Paths.get(args[0]).toAbsolutePath().toString().split("\\.")[0];
+        Writer.setFilePath(fileName + ".asm");
+        Writer.clearFile();
+        String headerPath = Paths.get("src/header.txt").toAbsolutePath().toString();
+        Writer.writeHeader(headerPath);
+        String footerPath = Paths.get("src/footer.txt").toAbsolutePath().toString();
         PrePro preP = new PrePro();
         Parser p = new Parser(preP.filter(content));
         p.run();
+        Writer.writeFooter(footerPath);
     }
 }
